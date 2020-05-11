2ca9547ae82874a041e8538e48900f5b6caf1c9c
VladislavAslanov <vladislav.aslanov@482.solutions>
2020-05-11 15:31:44 +0300

    Fixed logo

--- src/components/containers/Header/index.js
+++ src/components/containers/Header/index.js
@@ -4,7 +4,7 @@ import { Col, Row } from "antd";
 import cn from "classnames";
 import "./style.css";
 import { ChangePassword, Profile, Logout } from '../../containers';
-import { logoRow } from '../../../assets/images/logoRow.svg';
+import logoRow from '../../../assets/images/logoRow.svg';
 import { actions } from '../../../state-management';
 
 class Header extends Component {
@@ -24,7 +24,7 @@ class Header extends Component {
           "flex-start": isLoggedIn,
           "flex-center": !isLoggedIn
         })}>
-          <logoRow/>
+          <img src={logoRow} alt="Woden logo"/>
         </Col>
         {
           isLoggedIn && (
