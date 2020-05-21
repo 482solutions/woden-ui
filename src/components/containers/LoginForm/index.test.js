de444b74fec44475fb12fbc08a68e8191a069db5
boivlad <boyvlad99@gmail.com>
2020-05-21 16:15:10 +0300

    add tests for LoginForm and NewFolder

--- src/components/containers/LoginForm/index.test.js
+++ src/components/containers/LoginForm/index.test.js
@@ -1,50 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { act } from 'react-dom/test-utils';
-import thunk from 'redux-thunk';
-import { createBrowserHistory } from 'history';
-import { Router } from 'react-router';
-import configureMockStore from 'redux-mock-store';
+import { Link } from 'react-router-dom';
+import Enzyme, { shallow } from 'enzyme';
+import { expect } from 'chai';
+import Adapter from 'enzyme-adapter-react-16';
+import { Button, Form, Input } from 'antd';
 import LoginForm from './index';
 
-let container;
-const middlewares = [thunk];
-const mockStore = configureMockStore(middlewares);
-const history = createBrowserHistory();
+Enzyme.configure({ adapter: new Adapter() });
 
-beforeEach(() => {
-  container = document.createElement('div');
-  document.body.appendChild(container);
-});
-
-afterEach(() => {
-  document.body.removeChild(container);
-  container = null;
-});
-
-window.matchMedia = window.matchMedia || function() {
-  return {
-    matches: false,
-    addListener() {
-    },
-    removeListener() {
-    },
-  };
-};
-
-it('renders without crashing', () => {
-  act(() => {
-    ReactDOM.render(
-      <Router history={history}>
-        <LoginForm/>
-      </Router>,
-      container,
-    );
-  });
-  const loginElement = container.getElementsByClassName('ant-form');
-  const inputs = container.getElementsByTagName('input');
-
-  expect(loginElement.length).toBe(1);
-  expect(inputs[0].id).toBe('name');
-  expect(inputs[1].id).toBe('password');
+it('Renders "LoginForm" check all components', () => {
+  const wrapper = shallow(<LoginForm/>);
+  expect(wrapper.find(Button)).to.have.lengthOf(1);
+  expect(wrapper.find(Form)).to.have.lengthOf(1);
+  expect(wrapper.find(Input)).to.have.lengthOf(1);
+  expect(wrapper.find(Input.Password)).to.have.lengthOf(1);
+  expect(wrapper.find(Link)).to.have.lengthOf(1);
 });
