e36afda72a1febe23067b0cde884d7137c21ffe7
VladislavAslanov <vladislav.aslanov@482.solutions>
2020-05-21 16:08:56 +0300

    Write test for Header

--- src/components/containers/Header/index.test.js
+++ src/components/containers/Header/index.test.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import { expect } from 'chai';
 import Adapter from 'enzyme-adapter-react-16';
-import Header from './index';
-import { Search } from '../../presentations';
+import { Header } from './index';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 it('Render without crashing', () => {
-  const wrapper = shallow(<Header/>);
-  expect(wrapper.find(Search)).to.have.lengthOf(1);
+  const wrapper = shallow(<Header isLoggedIn={true}/>);
+  expect(wrapper.find('#HeaderLogo')).to.have.lengthOf(1);
+  expect(wrapper.find('.search-block')).to.have.lengthOf(1);
+  expect(wrapper.find('.changePassword')).to.have.lengthOf(1);
+  expect(wrapper.find('.logout')).to.have.lengthOf(1);
+  expect(wrapper.find('.profile')).to.have.lengthOf(1);
 });
