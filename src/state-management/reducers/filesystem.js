f670a7aeb6f59d4b3619186f6503948c97d8f73e
VladislavAslanov <vladislav.aslanov@482.solutions>
2020-05-30 11:53:56 +0300

    WGRND-626 : implement button Go Back and write test for it

--- src/state-management/reducers/filesystem.js
+++ src/state-management/reducers/filesystem.js
@@ -1,5 +1,5 @@
 import {
-  BACK, FORWARD, GET_FOLDER_DATA, SEARCH_FOLDER_FILE, SET_FOLDER_DATA, GET_VERSIONS,
+  BACK, FORWARD, GET_FOLDER_DATA, SEARCH_FOLDER_FILE, SET_FOLDER_DATA,
 } from '../types';
 
 const initialState = {
@@ -8,10 +8,6 @@ const initialState = {
   parentHash: null,
   entryFolders: [],
   entryFiles: [],
-  versions: {
-    file: null,
-    versionList: [],
-  },
 };
 
 const handleBack = () => ({
@@ -20,16 +16,17 @@ const handleBack = () => ({
 const handleForward = () => ({
   // TODO: Реализация для обработки перехода в следующую директорию
 });
-const handleSetFolder = (state, folderData) => ({
-  ...state,
-  folderName: folderData.name,
-  folderHash: folderData.hash,
-  parentHash: folderData.parenthash,
-  entryFolders: folderData.folders,
-  entryFiles: folderData.files,
-});
+const handleSetFolder = (state, folderData) => (
+  {
+    ...state,
+    folderName: folderData.name,
+    folderHash: folderData.hash,
+    parentHash: folderData.parenthash,
+    entryFolders: folderData.folders,
+    entryFiles: folderData.files,
+  }
+);
 const handleGetFolder = (state, folderData) => ({
-  ...state,
   folderName: folderData.name,
   folderHash: folderData.hash,
   parentHash: folderData.parenthash,
@@ -37,19 +34,11 @@ const handleGetFolder = (state, folderData) => ({
   entryFiles: folderData.files,
 });
 const handleSearch = (state, resultData) => ({
-  ...state,
   folderName: resultData.name,
   folderHash: resultData.hash,
   parentHash: null,
   entryFolders: resultData.folders,
   entryFiles: resultData.files,
-});
-const handleVersions = (state, fileData) => ({
-  ...state,
-  versions: {
-    file: fileData.hash,
-    versionList: fileData.versionList,
-  },
 });
 const handlers = {
   [BACK]: handleBack,
@@ -57,7 +46,6 @@ const handlers = {
   [SET_FOLDER_DATA]: handleSetFolder,
   [GET_FOLDER_DATA]: handleGetFolder,
   [SEARCH_FOLDER_FILE]: handleSearch,
-  [GET_VERSIONS]: handleVersions,
 };
 
 export default (state = initialState, action) => {
