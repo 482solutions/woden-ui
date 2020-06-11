85040910891c3fc62da4dc0b3c00a8f5c8e95139
VladislavAslanov <vladislav.aslanov@482.solutions>
2020-06-11 18:32:02 +0300

    WGRND-648 : refactor of code

--- src/state-management/actions/auth.js
+++ src/state-management/actions/auth.js
@@ -53,7 +53,6 @@ const registration = async(user, dispatch) => {
 export const regRequest = (user) => async(dispatch) => registration(user, dispatch);
 
 const logIn = async(user, dispatch) => {
-  message.destroy();
   message.loading('Logging In...', 0);
   const password = (encryptData(user.password));
   const body = new Woden.Login();
