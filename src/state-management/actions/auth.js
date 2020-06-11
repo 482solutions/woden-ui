be089ce4bf2aafbee7253b59de914e5a1abf5728
VladislavAslanov <vladislav.aslanov@482.solutions>
2020-06-11 16:24:43 +0300

    WGRND-648 : add loading spinner for Log In

--- src/state-management/actions/auth.js
+++ src/state-management/actions/auth.js
@@ -53,6 +53,8 @@ const registration = async(user, dispatch) => {
 export const regRequest = (user) => async(dispatch) => registration(user, dispatch);
 
 const logIn = async(user, dispatch) => {
+  message.destroy();
+  message.loading('Logging In...');
   const password = (encryptData(user.password));
   const body = new Woden.Login();
   body.login = user.name;
@@ -61,6 +63,7 @@ const logIn = async(user, dispatch) => {
   body.privateKey = user.privateKey;
   api.login(
     body, (error, data, response) => {
+      message.destroy();
       if (error) {
         message.error(response.body.message);
       } else if (response.status === 200 && response.body.token) {
