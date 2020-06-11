988e986db183f0f550a4ae949d80abb4fe058ddc
VladislavAslanov <vladislav.aslanov@482.solutions>
2020-06-11 17:49:22 +0300

    WGRND-655 : create spinner for Get versions

--- src/state-management/actions/filesystem.js
+++ src/state-management/actions/filesystem.js
@@ -139,10 +139,12 @@ export const downloadFile = (cid, hash) => async(dispatch) => {
   );
 };
 export const getVersions = (hash) => async(dispatch) => {
+  message.loading('Getting file versions...');
   Bearer.apiKey = await getTokenForHeader();
   api.versions(
     hash,
     (error, data, response) => {
+      message.destroy();
       if (error) {
         message.error(response.body.message);
       } else {
