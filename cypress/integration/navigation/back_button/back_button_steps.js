be9d386ee9a6522d4bf19eae7f85556e3e132f72
ababenko <alina.babenko@482.solutions>
2020-06-11 17:23:45 +0300

    WGRND-622: added tests for "Back button" and update commands.js

--- cypress/integration/navigation/back_button/back_button_steps.js
+++ cypress/integration/navigation/back_button/back_button_steps.js
@@ -4,22 +4,22 @@ before(() => {
     cy.registerUser()
 })
 
-Given(/^Create folder with name "([^"]*)" in root without UI$/, function (folder) {
-    cy.createFolder(folder, 'root')
+Given(/^Create folder with name "([^"]*)" in root without UI$/,  (folder) => {
+    cy.createFolderInRoot(folder)
     cy.reload().wait(2000)
 });
 
-Given(/^Create folder with name "([^"]*)" in Folder_level_1$/, function (folder2) {
-    cy.createFolder(folder2, 'Folder_level_1')
+Given(/^Create folder with name "([^"]*)" in Folder_level_1$/, (folder2) => {
+    cy.createFolderInFolder(folder2, 'Folder_level_1')
     cy.reload().wait(2000)
 });
 
-Given(/^The user opened folder "([^"]*)" from the root folder$/, function (createdFolder) {
+Given(/^The user opened folder "([^"]*)" from the root folder$/, (createdFolder) => {
     cy.contains(createdFolder).dblclick().wait(1000)
 });
 
 When(/^The user press the back button$/, function () {
-    cy.get('.goBack').click().wait(1000)
+    cy.get('.goBack').click().wait(2000)
 });
 
 Then(/^The User is transferred to the root folder$/, function () {
@@ -29,7 +29,7 @@ Then(/^The Home and Back button becomes inactive$/, function () {
     cy.get('.goBack').should('not.be.visible')
 });
 Given(/^The user opened folder "([^"]*)" in Folder_level_1$/, function (createdFol) {
-    cy.contains(createdFol).dblclick().wait(1000)
+    cy.contains(createdFol).dblclick().wait(2000)
 });
 Then(/^The User is transferred to the previous folder 1 level back$/, function () {
     cy.get('.currentFolder').should('contain.text', 'Folder_level_1')
