6bcbb42e61868532f66b7057f4c12d08905eaec7
ababenko <alina.babenko@482.solutions>
2020-06-12 11:30:40 +0300

    WGRND-622: update

--- cypress/integration/navigation/back_button/back_button_steps.js
+++ cypress/integration/navigation/back_button/back_button_steps.js
@@ -10,22 +10,23 @@ Given(/^Create folder with name "([^"]*)" in Folder_level_1$/, (folder2) => {
 });
 
 Given(/^The user opened folder "([^"]*)" from the root folder$/, (createdFolder) => {
-    cy.contains(createdFolder).dblclick().wait(1000)
+    cy.contains(createdFolder).dblclick().wait(2000)
 });
 
-When(/^The user press the back button$/, function () {
-    cy.get('.goBack').click().wait(2000)
+When(/^The user press the back button$/,() => {
+    cy.get('.goBack').click().wait(3000)
 });
 
-Then(/^The User is transferred to the root folder$/, function () {
+Then(/^The User is transferred to the root folder$/, () => {
     cy.inRootFolder()
 });
-Then(/^The Home and Back button becomes inactive$/, function () {
+Then(/^The Home and Back button becomes inactive$/, () => {
     cy.get('.goBack').should('not.be.visible')
 });
-Given(/^The user opened folder "([^"]*)" in Folder_level_1$/, function (createdFol) {
-    cy.contains(createdFol).dblclick().wait(2000)
+Given(/^The user opened folder "([^"]*)" in Folder_level_1$/, (createdFol) => {
+    cy.contains(createdFol).dblclick().wait(3000)
 });
-Then(/^The User is transferred to the previous folder 1 level back$/, function () {
+Then(/^The User is transferred to the previous folder 1 level back$/, () => {
+    cy.wait(3000)
     cy.get('.currentFolder').should('contain.text', 'Folder_level_1')
 });
