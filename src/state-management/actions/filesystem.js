2760c2f54ab7594a2d550980ddb5e78ed27e3ca9
linababenko <62387045+linababenko@users.noreply.github.com>
2020-07-20 16:51:16 +0300

    Update filesystem.js

--- src/state-management/actions/filesystem.js
+++ src/state-management/actions/filesystem.js
@@ -151,7 +151,7 @@ export const updateFile = (file) => async() => {
 export const downloadFile = (hash, cid, name) => async(dispatch) => {
   message.loading('Downloading file...', 0);
   const token = await getTokenForHeader();
-  axios.get(`http://192.168.88.42:1823/api/v1/file/${hash}/${cid}`,
+  axios.get(`http://localhost:1823/api/v1/file/${hash}/${cid}`,
     {
       headers: { Authorization: token, 'Access-Control-Allow-Origin': '*' },
       responseType: 'blob',
