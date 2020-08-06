053d54ce508d6176b4d4f6d08d0b684524ddde0f
Daniil <black3phoenix@gmail.com>
2020-08-06 17:51:46 +0300

    add voting modal and voting results modal

---
+++ src/components/containers/VotingModal/index.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { message, Modal, Row, Button } from 'antd';
+import  { VotingModalSuccess }  from '../VotingModalSuccess/index.js';
+
+export function VotingModal(variants, votingName, voters, versionTime){
+    const [voted, setVoted] = useState(false);
+    const [btnText, setBtnText] = useState('Submit Your Vote'.toUpperCase());
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        setBtnText(('Vote For:' + e.target.value).toUpperCase());
+    }
+    
+    return !voted ? ( 
+        <Modal
+          centered={true}
+          okText={btnText}
+          >
+            <div className={"modal-size"}>
+              <Row>
+                <h3 className={"voting-success-title"}>Voting</h3>
+              </Row>
+              <Row>
+                <h4 className={"voting-success-message"}>{votingName}</h4>
+                <p>{versionTime}</p>
+              </Row>
+              <Row>
+                  {variants.forEach((variant) => {
+                  <Button onClick={() => handleClick(e)}>{variant}</Button>
+                  })}
+              </Row>
+              <Row>
+                  <Button onClick={() => setVoted(!voted)} />
+              </Row>
+            </div>)
+          </Modal>
+        ) : (
+         VotingModalSuccess('Congratulations!', 'Your vote is in')
+        )
+}
